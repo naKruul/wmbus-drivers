1028967568cc70ab45abd77b935cba2df348874e
naKruul <40398952+naKruul@users.noreply.github.com>
2024-06-09 01:29:17 +0200

    Update driver_lse.h

--- driver_lse.h
+++ driver_lse.h
@@ -18,13 +18,9 @@ struct LseWaterMeter : Driver
         std::map<std::string, double> ret_val{};
 
         // Fügen Sie die spezifischen Methoden hinzu, um die Werte zu extrahieren
-        add_to_map(ret_val, "total_m3", this->get_total(telegram));
-        add_to_map(ret_val, "due_date_m3", this->get_due_date_volume(telegram));
-        add_to_map(ret_val, "due_date", this->get_due_date(telegram));
-        add_to_map(ret_val, "error_code", this->get_error_code(telegram));
-        add_to_map(ret_val, "error_date", this->get_error_date(telegram));
-        add_to_map(ret_val, "device_date_time", this->get_device_date_time(telegram));
-        add_to_map(ret_val, "timestamp", this->get_timestamp(telegram));
+        add_to_map(ret_val, "total_m3", this->0C13(telegram));
+        add_to_map(ret_val, "due_date_m3", this->426C(telegram));
+        add_to_map(ret_val, "due_date", this->4C13(telegram));
 
         if (ret_val.size() > 0) {
             return ret_val;
